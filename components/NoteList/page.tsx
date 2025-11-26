2327187e57d4a71b1e9691b46d959dbf91cb4469
pona <ipona3710@gmail.com>
2025-11-26 16:16:51 +0200

    added components

---
+++ components/NoteList/page.tsx
@@ -0,0 +1,38 @@
+import type { Note } from '../../types/note';
+import css from './NoteList.module.css';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteNote } from '../../services/noteService';
+
+interface NoteListProps {
+  notes: Note[];
+}
+
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (id: string) => deleteNote(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] }); // оновлюємо кеш
+    },
+  });
+
+  if (!notes.length) return null;
+
+  return (
+    <ul className={css.list}>
+      {notes.map(note => (
+        <li key={note.id} className={css.listItem}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            <button className={css.button} onClick={() => mutation.mutate(note.id)}>
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
