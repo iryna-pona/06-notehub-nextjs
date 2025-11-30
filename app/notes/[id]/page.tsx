4706a24de3431e68bdc88be9860f70300c70c430
iryna-pona <ipona3710@gmail.com>
2025-11-30 23:27:49 +0200

    fixed layout, notes/[id]/page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   dehydrate,
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NoteDetailsClient from "./NoteDetails.client";
+import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
 
 type NoteProps = {
     params: Promise<{id: string}>
